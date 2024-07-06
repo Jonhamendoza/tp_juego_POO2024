572c89d635f3b3db64edba7b7eae0a9f8cbcdd53
Jonhamendoza <94213113+Jonhamendoza@users.noreply.github.com>
2024-07-06 01:31:25 -0300

    Update nivel1.h

--- TP_2/Simulador_de_Control_Fronterizo/nivel1.h
+++ TP_2/Simulador_de_Control_Fronterizo/nivel1.h
@@ -1,8 +1,9 @@
-#ifndef NIVEL1_H
+ifndef NIVEL1_H
 #define NIVEL1_H
 #include "nivel.h"
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 
 class Nivel1 : public Nivel
@@ -10,7 +11,7 @@ class Nivel1 : public Nivel
 public:
     Nivel1();
     std::string* getReglas();
-    std::string* getSolicitudes();
+    void getSolicitudes(std::string Solicitudes[], int &aux_TipoPersona);
 };
 
 #endif // NIVEL1_H
