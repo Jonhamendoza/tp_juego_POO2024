51afb9b76d128846a7b66b006ce00cc2fa502264
Jonhamendoza <94213113+Jonhamendoza@users.noreply.github.com>
2024-07-07 19:27:12 -0300

    Update main.cpp

--- TP_2/Simulador_de_Control_Fronterizo/main.cpp
+++ TP_2/Simulador_de_Control_Fronterizo/main.cpp
@@ -17,13 +17,35 @@ int main()
     cout<<"Bienvenido al juego. \n";
     cout<<"Ingrese su nombre: \n";
     getline(cin,nombreJugador);
+
+    // CREACION DE VALORES //
+
     int Puntuacion=0;
     int Decision;
     int Multa=0;
     Jugador jugador(nombreJugador);
+
+    // CREACION DE OBJETOS NIVELES //
+
     Nivel1 nivelInicial;
     string* reglasNivel1 = new string [6];
     reglasNivel1=nivelInicial.getReglas();
+    //NIVEL2
+    Nivel2 nivel2;
+    string* reglasNivel2 = new string [6];
+    reglasNivel2=nivel2.getReglas();
+    //NIVEL3
+    Nivel3 nivel3;
+    string* reglasNivel3 = new string [6];
+    reglasNivel3=nivel3.getReglas();
+    //NIVEL4
+    Nivel4 nivel4;
+    string* reglasNivel4 = new string [6];
+    reglasNivel4=nivel4.getReglas();
+    //NIVEL FINAL
+    NivelFinal nivelFinal;
+    string* reglasNivelFinal = new string [6];
+    reglasNivelFinal=nivelFinal.getReglas();
 
     cout<<endl;
     cout<<endl;
@@ -48,12 +70,12 @@ int main()
         for(int i=0; i<30;i++){
             cout<<Solicitudes[i]<<endl;
         }
-        cout<<Solicitudes[22];
+
         cout<<endl;
         cout<<endl;
        //
 
-       //delete[] Solicitudes;
+
         Decision=jugador.tomarDecision();
         if(Decision==1){
             switch(auxTipoPersona){
@@ -90,6 +112,282 @@ int main()
             }
         }
     }
-    while ((Puntuacion<100) ) ;
+    while ((Puntuacion >=0) && (Puntuacion<50) && (Multa < 4)) ;
+    if((Puntuacion<0) || (Multa >= 4)){
+        cout<<"Juego terminado.\n";
+        cout<<"Puntuacion: "<<Puntuacion<<endl;
+        cout<<"Cantidad de Multas recibidas: "<<Multa<<endl;
+    }
+
+    for(int i=0;i<6;i++){
+        cout<<reglasNivel2[i]<<endl;
+    }
+    delete [] reglasNivel2;
+    cout<<endl;
+
+    do {
+
+        cout<<"Su puntuacion es: "<<Puntuacion<<endl;
+        string Solicitudes[30];
+
+
+       nivel2.getSolicitudes(Solicitudes, auxTipoPersona);
+       // cout<<"aca!!\n";
+        for(int i=0; i<30;i++){
+            cout<<Solicitudes[i]<<endl;
+        }
+
+        cout<<endl;
+        cout<<endl;
+       //
+
+
+        Decision=jugador.tomarDecision();
+        if(Decision==1){
+            switch(auxTipoPersona){
+            case 1:
+                Puntuacion+=10;
+                break;
+            case 2:
+                Puntuacion+=15;
+                break;
+            case 3:
+                Puntuacion+=25;
+                break;
+            case 4:
+                Puntuacion-=25;
+                Multa++;
+                break;
+            }
+
+        }else if(Decision==2){
+            switch(auxTipoPersona){
+            case 1:
+                Puntuacion-=15;
+                break;
+            case 2:
+                Puntuacion-=25;
+                break;
+            case 3:
+                Puntuacion-=25;
+                Multa++;
+                break;
+            case 4:
+                Puntuacion-=0;
+                break;
+            }
+        }
+    }
+    while ((Puntuacion >=50) && (Puntuacion<100) && (Multa < 4)) ;
+    if((Puntuacion<50) || (Multa >= 4)){
+        cout<<"Juego terminado.\n";
+        cout<<"Puntuacion: "<<Puntuacion<<endl;
+        cout<<"Cantidad de Multas recibidas: "<<Multa<<endl;
+    }
+
+    for(int i=0;i<6;i++){
+        cout<<reglasNivel3[i]<<endl;
+    }
+    delete [] reglasNivel3;
+    cout<<endl;
+
+    do {
+
+        cout<<"Su puntuacion es: "<<Puntuacion<<endl;
+        string Solicitudes[30];
+
+
+       nivel3.getSolicitudes(Solicitudes, auxTipoPersona);
+       // cout<<"aca!!\n";
+        for(int i=0; i<30;i++){
+            cout<<Solicitudes[i]<<endl;
+        }
+
+        cout<<endl;
+        cout<<endl;
+       //
+
+
+        Decision=jugador.tomarDecision();
+        if(Decision==1){
+            switch(auxTipoPersona){
+            case 1:
+                Puntuacion+=10;
+                break;
+            case 2:
+                Puntuacion+=15;
+                break;
+            case 3:
+                Puntuacion+=25;
+                break;
+            case 4:
+                Puntuacion-=25;
+                Multa++;
+                break;
+            }
+
+        }else if(Decision==2){
+            switch(auxTipoPersona){
+            case 1:
+                Puntuacion-=15;
+                break;
+            case 2:
+                Puntuacion-=25;
+                break;
+            case 3:
+                Puntuacion-=25;
+                Multa++;
+                break;
+            case 4:
+                Puntuacion-=0;
+                break;
+            }
+        }
+    }
+    while ((Puntuacion >=100) && (Puntuacion<150) && (Multa < 4)) ;
+    if((Puntuacion<100) || (Multa >= 4)){
+        cout<<"Juego terminado.\n";
+        cout<<"Puntuacion: "<<Puntuacion<<endl;
+        cout<<"Cantidad de Multas recibidas: "<<Multa<<endl;
+    }
+
+    for(int i=0;i<6;i++){
+        cout<<reglasNivel4[i]<<endl;
+    }
+    delete [] reglasNivel4;
+    cout<<endl;
+
+    do {
+
+        cout<<"Su puntuacion es: "<<Puntuacion<<endl;
+        string Solicitudes[30];
+
+
+       nivel4.getSolicitudes(Solicitudes, auxTipoPersona);
+       // cout<<"aca!!\n";
+        for(int i=0; i<30;i++){
+            cout<<Solicitudes[i]<<endl;
+        }
+
+        cout<<endl;
+        cout<<endl;
+       //
+
+
+        Decision=jugador.tomarDecision();
+        if(Decision==1){
+            switch(auxTipoPersona){
+            case 1:
+                Puntuacion+=10;
+                break;
+            case 2:
+                Puntuacion+=15;
+                break;
+            case 3:
+                Puntuacion+=25;
+                break;
+            case 4:
+                Puntuacion-=25;
+                Multa++;
+                break;
+            }
+
+        }else if(Decision==2){
+            switch(auxTipoPersona){
+            case 1:
+                Puntuacion-=15;
+                break;
+            case 2:
+                Puntuacion-=25;
+                break;
+            case 3:
+                Puntuacion-=25;
+                Multa++;
+                break;
+            case 4:
+                Puntuacion-=0;
+                break;
+            }
+        }
+    }
+    while ((Puntuacion >=150) && (Puntuacion<200) && (Multa < 4)) ;
+    if((Puntuacion<150) || (Multa >= 4)){
+        cout<<"Juego terminado.\n";
+        cout<<"Puntuacion: "<<Puntuacion<<endl;
+        cout<<"Cantidad de Multas recibidas: "<<Multa<<endl;
+    }
+
+    for(int i=0;i<6;i++){
+        cout<<reglasNivelFinal[i]<<endl;
+    }
+    delete [] reglasNivelFinal;
+    cout<<endl;
+
+    do {
+
+        cout<<"Su puntuacion es: "<<Puntuacion<<endl;
+        string Solicitudes[30];
+
+
+       nivelFinal.getSolicitudes(Solicitudes, auxTipoPersona);
+       // cout<<"aca!!\n";
+        for(int i=0; i<30;i++){
+            cout<<Solicitudes[i]<<endl;
+        }
+
+        cout<<endl;
+        cout<<endl;
+       //
+
+
+        Decision=jugador.tomarDecision();
+        if(Decision==1){
+            switch(auxTipoPersona){
+            case 1:
+                Puntuacion+=10;
+                break;
+            case 2:
+                Puntuacion+=15;
+                break;
+            case 3:
+                Puntuacion+=25;
+                break;
+            case 4:
+                Puntuacion-=25;
+                Multa++;
+                break;
+            }
+
+        }else if(Decision==2){
+            switch(auxTipoPersona){
+            case 1:
+                Puntuacion-=15;
+                break;
+            case 2:
+                Puntuacion-=25;
+                break;
+            case 3:
+                Puntuacion-=25;
+                Multa++;
+                break;
+            case 4:
+                Puntuacion-=0;
+                break;
+            }
+        }
+    }
+    while ((Puntuacion >=200) && (Puntuacion<250) && (Multa < 4)) ;
+    if((Puntuacion<200) || (Multa >= 4)){
+        cout<<"Juego terminado.\n";
+        cout<<"Puntuacion: "<<Puntuacion<<endl;
+        cout<<"Cantidad de Multas recibidas: "<<Multa<<endl;
+    } else {
+        cout<<"Felicidades "<<nombreJugador <<" GANASTE\n";
+        cout<<"Puntuacion: "<<Puntuacion<<endl;
+        cout<<"Cantidad de Multas recibidas: "<<Multa<<endl;
+    }
+
+
+
     return 0;
 }
